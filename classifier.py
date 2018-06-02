d2014a8d4082979ba7ef84bce908627fe4d34299
Simon Accascina <simon@accascina.me>
2018-06-02 23:22:51 +0200

    Implement manual "classifier"

--- classifier.py
+++ classifier.py
@@ -1,3 +1,19 @@
+from psychopy import event
+from configuration import CONF
+
 class Classifier:
     def get_prediction(self):
-        return 'left'
+        return getattr(self, '_get_prediction_{}'.format(CONF['input']['method']))()
+
+    def _get_prediction_manual(self):
+        while True:
+            key = event.waitKeys()
+            for key_type, keys in CONF['keys'].items():
+                if key[0] in keys:
+                    return key_type
+
+    def _get_prediction_auto(self):
+        pass
+
+    def _get_prediction_satori(self):
+        pass
