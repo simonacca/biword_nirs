da47f97d08144626af8687834532f62b2d82a624
Simon Accascina <simon@accascina.me>
2018-06-02 22:45:30 +0200

    Add error correction capability

--- dataset.py
+++ dataset.py
@@ -1,3 +1,6 @@
+import copy
+
+
 class Dataset:
     "This class handles all of the datset manipulation."
     def __init__(self, dataset_name, to_clean=True):
@@ -8,6 +11,8 @@ class Dataset:
         self._load_dataset(dataset_name)
         if to_clean:
             self._clean_dataset()
+        self.directions = []
+        self.mistake_count = 0
 
     def _load_dataset(self, dataset_name):
         """
@@ -29,21 +34,42 @@ class Dataset:
         self.dataset = list(set(self.dataset))
         self.dataset = sorted(self.dataset)
 
-    def _middle_word_position(self):
+    def _middle_word_position(self, dataset):
         "Internally used to get the position of the middle word."
-        return int(len(self.dataset)/2)
+        return int(len(dataset)/2)
 
     def middle_word(self):
-        "Internally used to get the middle word of the dataset."
-        return self.dataset[self._middle_word_position()]
+        "Used to get the middle word of the dataset."
+        t = copy.deepcopy(self.dataset)
+        for direction in self.directions:
+            if direction == 'before':
+                t = t[:self._middle_word_position(t)]
+            elif direction == 'after':
+                t = t[self._middle_word_position(t):]
+        return t[0], t[self._middle_word_position(t)], t[-1]
 
-    def split_dataset(self, direction):
-        "Splits the dataset based on answer key."
-        if direction == "before":
-            self.dataset = self.dataset[:self._middle_word_position()]
-        elif direction == "after":
-            self.dataset = self.dataset[self._middle_word_position():]
+
+    def split(self, direction):
+        "Splits the dataset based on answer."
+
+        if direction == 'correction':
+            try:
+                old_direction = self.directions.pop()
+                self.mistake_count += 1
+            except IndexError:
+                return
+            
+            if self.mistake_count == 1:
+                self.directions.append(
+                    'before' if old_direction == 'after' else 'after')
+                return
+            elif self.mistake_count == 2:
+                self.mistake_count = 0
+        else:
+            self.directions.append(direction)
+            self.mistake_count = 0
 
     def is_finished(self):
         "Checks for the end of the process, when there is only 1 word left."
-        return len(self.dataset) == 1
+        left, middle, right = self.middle_word()
+        return left == middle == right
