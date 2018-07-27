588f81ee7c85b29e446ab9d0d8e25dd6d1d68895
Simon Accascina <simon@accascina.me>
2018-07-27 12:15:57 +0200

    Implement escape

--- classifier.py
+++ classifier.py
@@ -1,11 +1,20 @@
+import sys
 import operator
 from psychopy import event
 from configuration import CONF
 import satori
 
+
 class Classifier:
     def __init__(self, dataset):
         self.dataset = dataset
+
+    def _escape(self, keys=None):
+        if not keys:
+            keys = event.getKeys()
+        if keys and keys[-1] == 'escape':
+            print('User terminated run early')
+            sys.exit(1)
 
 
     def get_prediction(self, sequence_number):
@@ -14,15 +23,18 @@ class Classifier:
 
     def _get_prediction_manual(self, sequence_number):
         key = event.getKeys()
+        self._escape(key)
         for direction, keys in CONF['keys'].items():
-            if key and (key[0] in keys):
+            if key and (key[-1] in keys):
                 return direction
         return None
 
     def _get_prediction_auto(self, sequence_number):
+        self._escape()
         return 'before' if CONF['input']['target'] < self.dataset.middle_word()[1] else 'after'
 
     def _get_prediction_satori(self, sequence_number):
+        self._escape()
         data = satori.parse(CONF['satori']['filepath'])
         # Maybe try again here?
         try:
